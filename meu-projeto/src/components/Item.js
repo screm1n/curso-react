9afa18f7c46c980be5d78c1ca2a746b47bbac58f
Octávio <screminoctavio@gmail.com>
2024-03-13 17:23:30 -0300

    props avançado

--- meu-projeto/src/components/Item.js
+++ meu-projeto/src/components/Item.js
@@ -1,9 +1,23 @@
-function Item(props) {
+import PropTypes from 'prop-types'
+
+function Item( {marca, lancamento } ) {
     return (
         <>
-            <li>{props.marca}</li>
+        <li>
+          {marca} - {lancamento}
+        </li>
         </>
     )
+}
+
+Item.propTypes = { 
+    marca: PropTypes.string.isRequired,
+    lancamento: PropTypes.number,
+}
+
+Item.defaultProps = {
+    marca: 'Faltou a marca',
+    lancamento: 0,
 }
 
 export default Item
--- meu-projeto/src/components/List.js
+++ meu-projeto/src/components/List.js
@@ -5,9 +5,9 @@ function List() {
         <> 
             <h1>Minha Lista</h1>
             <ul>
-                <Item marca="Ferrari" />
-                <Item marca="Porsche" />
-                <Item marca="Lamborghini" />
+                <Item marca="Ferrari" lancamento={1953} />
+                <Item marca="Porsche" lancamento={1940} />
+                <Item marca="Lamborghini" lancamento={1920} />
             </ul>
         </>
     )
