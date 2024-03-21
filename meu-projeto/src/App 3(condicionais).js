9b7bf035e212f9ba73ff65842f820ecb0e65a86c
Octávio <screminoctavio@gmail.com>
2024-03-21 17:52:13 -0300

    lista em react

---
+++ meu-projeto/src/App 3(condicionais).js
@@ -0,0 +1,14 @@
+import './App.css';
+import Condicional from './components/Condicional';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Renderização de Condicional</h1>
+      <Condicional />
+    </div>
+  )
+}
+
+export default App
---
+++ meu-projeto/src/components/OutraLista.js
@@ -0,0 +1,13 @@
+function OutraLista({ itens }) {
+    return ( 
+        <>
+            <h3>Lista de coisas boas:</h3>
+        
+                {itens.map((item) => (
+                    <p>{item}</p>
+                ))}
+        </>
+    )
+}
+
+export default OutraLista
