6ff0ac656201672602ad6d3bb57cf52a3f908fef
Octávio <screminoctavio@gmail.com>
2024-03-21 19:15:40 -0300

    aprendendo state lift react

---
+++ meu-projeto/src/App 3 (listas).js
@@ -0,0 +1,16 @@
+import './App.css';
+import OutraLista from './components/OutraLista';
+
+function App() {
+
+  const meusItens = ['React', 'Vue', 'Angular']
+
+  return (
+    <div className="App">
+      <h1>Renderização de Listas</h1>
+      <OutraLista itens={meusItens} />
+    </div>
+  )
+}
+
+export default App
---
+++ meu-projeto/src/components/Saudacao.js
@@ -0,0 +1,13 @@
+function Saudacao({ nome }) {
+    function gerarSaudacao(algumNome) {
+        return `Olá, ${algumNome}`
+    }
+
+    return (
+        <>
+            {nome && <p>{gerarSaudacao(nome)}.</p>}
+        </>
+    )
+}
+
+export default Saudacao
